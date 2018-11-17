b3f20fbeb5e62693d9310ee0602ea1a3e90c9918
Goeun Park <goeuntothepark@gmail.com>
2018-11-16 16:02:58 -0800

    put in document.ready

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-let setTimeout = function setTimeout(){
-  window.setTimeout(timer, 1000);
-};
+$(document).ready(function(){
+  let setTimeout = function setTimeout(){
+    window.setTimeout(timer, 1000);
+  };
 
-let timer = function timer() {
-  const time = new Date();
-  const clock = $('#clock');
+  let timer = function timer() {
+    const time = new Date();
+    const clock = $('#clock');
 
-  const hour = time.getHours();
-  const minute = time.getMinutes();
-  const second = time.getSeconds();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
 
-  clock.html(hour + ":" + minute + ":" + second);
-  // // this also works:
-  // clock.html(time.toLocaleTimeString());
-  
-  setTimeout();
-};
+    clock.html(hour + ":" + minute + ":" + second);
+    // // this also works:
+    // clock.html(time.toLocaleTimeString());
+
+    setTimeout();
+  };
 
 
-timer();
+  timer();
+
+});
