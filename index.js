a3402a354fdcf0d35e17d75de6b95af5c8f5c445
Goeun Park <goeuntothepark@gmail.com>
2018-11-16 16:01:51 -0800

    a functional clock

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+let setTimeout = function setTimeout(){
+  window.setTimeout(timer, 1000);
+};
+
+let timer = function timer() {
+  const time = new Date();
+  const clock = $('#clock');
+
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  clock.html(hour + ":" + minute + ":" + second);
+  // // this also works:
+  // clock.html(time.toLocaleTimeString());
+  
+  setTimeout();
+};
+
+
+timer();
